dab3dcee85716f3330736cae26e3187234da824d
jeanpfs <jean.pfs2@gmail.com>
2015-11-09 00:51:58 -0200

    Teste de versionamento de pacote.

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,5 +1,7 @@
 <?php
-
+/*
+ * Include all files of Route directory
+ */
 Route::group(['middleware' => 'cors'], function() {
     foreach (glob(__DIR__ . '/Routes/*.php') as $file) {
         require $file;
