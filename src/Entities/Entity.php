f020cea1dfbf3d04489378d8f96db08cd29f9b7b
jeanpfs <jean.pfs2@gmail.com>
2016-02-17 23:39:00 -0200

    Ajuste remocao de funcao de data na entidade

--- src/Entities/Entity.php
+++ src/Entities/Entity.php
@@ -7,16 +7,6 @@ class Entity extends Model
 {
 
     /**
-     * Return the default date format from database
-     *
-     * @return string
-     */
-    protected function getDateFormat()
-    {
-        return (config('database.default') == 'sqlsrv') ? 'M d Y H:iA' : 'Y-m-d H:i:s';
-    }
-
-    /**
      * Return the default date format to created at attribute
      *
      * @return string
