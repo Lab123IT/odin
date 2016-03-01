1e81ad55841977677b2f93306958e1459a129864
jeanpfs <jean.pfs2@gmail.com>
2016-03-01 10:33:57 -0300

    Removido formatacao de data para MsSQL

--- src/Entities/Entity.php
+++ src/Entities/Entity.php
@@ -5,34 +5,4 @@ use Illuminate\Database\Eloquent\Model;
 
 class Entity extends Model
 {
-
-    /**
-     * Return the default date format from database
-     *
-     * @return string
-     */
-    protected function getDateFormat()
-    {
-        return (config('database.default') == 'sqlsrv') ? 'M d Y H:iA' : 'Y-m-d H:i:s';
-    }
-
-    /**
-     * Return the default date format to created at attribute
-     *
-     * @return string
-     */
-    public function getCreatedAtAttribute($date)
-    {
-        return \DateTime::createFromFormat($this->getDateFormat(), $date)->format('d/m/Y H:i');
-    }
-
-    /**
-     * Return the default date format to updated at attribute
-     *
-     * @return string
-     */
-    public function getUpdatedAtAttribute($date)
-    {
-        return \DateTime::createFromFormat($this->getDateFormat(), $date)->format('d/m/Y H:i');
-    }
 }
